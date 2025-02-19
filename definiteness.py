f97d90de25934cbf8385653d690dc1d44f9f26a6
serban-hartular <81067211+serban-hartular@users.noreply.github.com>
2025-02-19 17:47:30 +0200

    Making sure it's up to date

--- definiteness.py
+++ definiteness.py
@@ -26,49 +26,50 @@ def what_determiner_en(node : cp.Tree) -> Definiteness:
     return Definiteness.BARE
 
 def get_noun_left_perifiery(noun : cp.Tree) -> list[cp.Tree]:
-    left_perifery = cp.Search('<[upos=DET,PRON,ADJ,ADP | deprel=det,case]').match(noun)
+    left_perifery = cp.Search('<[upos=DET,PRON,ADJ,ADP | deprel=det,case,cop]').match(noun)
     fixed = [cp.Search('/[deprel=fixed,flat]').match(child) for child in left_perifery]
     for f in fixed:
         left_perifery.extend(f)
     left_perifery.sort(key=lambda n : n.id())
     return left_perifery
 
-print('Loading...')
-noun_pairs = utils.p_load('./noun_equivalents_list.p')
-roDoc = cp.Doc.from_conllu('./ro1984-v1.conllu')
-enDoc = cp.Doc.from_conllu('./en1984-v1.conllu')
+if __name__ == "__main__":
+    print('Loading...')
+    noun_pairs = utils.p_load('./noun_equivalents_list.p')
+    roDoc = cp.Doc.from_conllu('./ro1984-v1.conllu')
+    enDoc = cp.Doc.from_conllu('./en1984-v1.conllu')
 
-limit = 1000
-count = 0
+    limit = 1000
+    count = 0
 
-for np in noun_pairs:
-    if count > limit:
-        break
-    i = np['chunk_index']
-    en_tok = np['src_data']['index']
-    en_uid = np['src_data']['uid']
-    en_node = enDoc.get_node(en_uid)
-    en_perifery = get_noun_left_perifiery(en_node)
-    en_string = ' '.join([n.sdata('form') for n in en_perifery + [en_node]])
-    en_def = what_determiner_en(en_node).value
-    en_deprel = en_node.sdata('deprel')
-    if 'index' in np['trgt_data']:
-        ro_tok = np['trgt_data']['index']
-        ro_uid = np['trgt_data']['uid']
-        ro_node = roDoc.get_node(ro_uid)
-        ro_perifery = get_noun_left_perifiery(ro_node)
-        ro_string = ' '.join([n.sdata('form') for n in ro_perifery + [ro_node]])
-        ro_def = what_determiner_ro(ro_node).value
-        ro_deprel = ro_node.sdata('deprel')
-    else:
-        ro_tok = ro_uid = ro_node = ro_def = ro_string = ro_deprel = None
-    if ro_def == en_def:
-        continue
-    data = [i, en_string, en_tok, en_def, en_deprel,
-            ro_string, ro_tok, ro_def, ro_deprel, '\t',
-            "'" + str(en_node.sentence()), "'" + str(ro_node.sentence()) if ro_node else None]
-    print('\t'.join([str(s) for s in data]))
-    count += 1
+    for np in noun_pairs:
+        if count > limit:
+            break
+        i = np['chunk_index']
+        en_tok = np['src_data']['index']
+        en_uid = np['src_data']['uid']
+        en_node = enDoc.get_node(en_uid)
+        en_perifery = get_noun_left_perifiery(en_node)
+        en_string = ' '.join([n.sdata('form') for n in en_perifery + [en_node]])
+        en_def = what_determiner_en(en_node).value
+        en_deprel = en_node.sdata('deprel')
+        if 'index' in np['trgt_data']:
+            ro_tok = np['trgt_data']['index']
+            ro_uid = np['trgt_data']['uid']
+            ro_node = roDoc.get_node(ro_uid)
+            ro_perifery = get_noun_left_perifiery(ro_node)
+            ro_string = ' '.join([n.sdata('form') for n in ro_perifery + [ro_node]])
+            ro_def = what_determiner_ro(ro_node).value
+            ro_deprel = ro_node.sdata('deprel')
+        else:
+            ro_tok = ro_uid = ro_node = ro_def = ro_string = ro_deprel = None
+        if ro_def == en_def:
+            continue
+        data = [i, en_string, en_tok, en_def, en_deprel,
+                ro_string, ro_tok, ro_def, ro_deprel, '\t',
+                "'" + str(en_node.sentence()), "'" + str(ro_node.sentence()) if ro_node else None]
+        print('\t'.join([str(s) for s in data]))
+        count += 1
 
 
 
